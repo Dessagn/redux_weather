1d0aaefea1378fcc9fc5b6c85f39052ed18c078d
Dessagn <temesgen.dessalegn@gmail.com>
2017-10-24 15:27:14 +0300

    Add cityAPI weather search

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'f73ba613a3b76837f1619a8bf18fd0e0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/containers/SearchBar.jsx
+++ src/containers/SearchBar.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Glyphicon, FormGroup, FormControl, InputGroup, InputControl, Button } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
   constructor(props) {
@@ -8,33 +12,33 @@ class SearchBar extends Component {
       term: ''
     };
   }
+
   handleChange = (e) => {
     let term = e.target.value;
-    this.setState({
-      term
-    });
-
+    this.setState({ term });
   };
+
   handleSearch= (e) =>{
     e.preventDefault();
-    console.log('Search Term is: ', this.state.term);
+    /**We need to go and fetch weather here by passing in the city*/
+    this.props.fetchWeather(this.state.term);
+    /**Just to make the search bar empty after search is entered, */
+    this.setState({term: ''});
   };
-  handleSubmit = (e) =>{
-    e.preventDefault();
-  }
+
   render() {
     return (
-      <form onSubmit={(e) => this.handleSubmit} className="search-bar">
+      <form
+        onSubmit={(e) => this.handleSearch(e)}
+        className="search-bar">
         <FormGroup controlId="searchText">
           <InputGroup>
             <FormControl
               type="text"
               value={this.state.term}
               placeholder="Search a City"
-              onChange={(e)=>this.handleChange(e)}
-              onKeyPress={(e)=>this.handleSearch(e)}
-           />
-           <InputGroup.Addon className="search-glyph" onClick={(e)=>this.handleSearch(e)}><Glyphicon glyph="search" /></InputGroup.Addon>
+              onChange={(e)=>this.handleChange(e)} />
+            <InputGroup.Addon className="search-glyph" onClick={(e) => this.handleSearch(e)}><Glyphicon glyph="search" /></InputGroup.Addon>
           </InputGroup>
         </FormGroup>
       </form>
@@ -42,4 +46,8 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
