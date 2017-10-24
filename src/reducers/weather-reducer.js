e5dcc25e99394235084e0237c736150b00c8b95f
Dessagn <temesgen.dessalegn@gmail.com>
2017-10-25 01:01:53 +0300

    Finish weather-reducer.js

--- src/reducers/weather-reducer.js
+++ src/reducers/weather-reducer.js
@@ -1,4 +1,11 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 export default function(state=[], action) {
-  console.log('Action Receaved!', action);
+  //console.log('Action Receaved!', action);
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state];
+      //SAME AS: state.concat[(action.payload.data)]
+  }
   return state;
 }
